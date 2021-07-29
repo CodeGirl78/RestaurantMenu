af723a6a6b55c96430e0d1517fdad207130c7ae0
Melinda <codegirl78@gmail.com>
2021-07-29 14:42:46 -0500

    Added a Menu class and added menu code

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        public string MenuItem { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+
+        public Menu(string menuItem, DateTime lastUpdated)
+        {
+            MenuItem = menuItem;
+            LastUpdated = lastUpdated;
+            //LastUpdated =  DateTime.Now ??
+        }
+    }
+}
